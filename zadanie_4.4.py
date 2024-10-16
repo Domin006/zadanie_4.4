dcb9853d0dca20b5d8da29fc62052f331862fc62
DominikJasek <dominikjasek94@gmail.com>
2024-10-16 19:40:26 +0200

    rozwiazanie

--- zadanie_4.4.py
+++ zadanie_4.4.py
@@ -15,3 +15,17 @@ print("1.Dodawanie")
 print("2.Odejmowanie")
 print("3.Mnożenie")
 print("4.Dzielenie")
+# wybór działania
+wybor = input("Wprowadź wybór(1/2/3/4): ")
+# sprawdzanie wyboru
+if wybor in ('1', '2', '3', '4', '5'):
+    liczba1 = float(input("Wprowadź pierwszą liczbę: "))
+    liczba2 = float(input("Wprowadź drugą liczbę: "))
+    if wybor == '1':
+        print(liczba1, "+", liczba2, "=", dodawanie(liczba1, liczba2))
+    elif wybor == '2':
+        print(liczba1, "-", liczba2, "=", odejmowanie(liczba1, liczba2))
+    elif wybor == '3':
+        print(liczba1, "*", liczba2, "=", mnozenie(liczba1, liczba2))
+    elif wybor == '4':
+        print(liczba1, "/", liczba2, "=", dzielenie(liczba1, liczba2))
